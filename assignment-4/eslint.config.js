bb9089b4ac65489ccf9afc327fae4a851d8b0b54
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-29 17:36:00 +0600

    project init + file structure + dependencies

---
+++ assignment-4/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules:{
+       "no-restricted-imports": "off",
+         "@typescript-eslint/no-restricted-imports": [
+        "warn",
+        {
+          name: "react-redux",
+          importNames: ["useSelector", "useDispatch"],
+          message:
+            "Use typed hooks `useAppDispatch` and `useAppSelector` instead.",
+        },
+      ],
+
+    }
+  },
+])
