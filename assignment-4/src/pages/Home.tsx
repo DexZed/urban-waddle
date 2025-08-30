731dcfe72af2009f34c15d6e01e6d490b7975cbd
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-30 16:32:46 +0600

    `Added hero section and navigation link to Home page`

--- assignment-4/src/pages/Home.tsx
+++ assignment-4/src/pages/Home.tsx
@@ -1,9 +1,36 @@
-
-
-
+import { Link } from "react-router";
 
 function Home() {
-  return <><div>Home</div></>;
+  return (
+    <>
+      <div
+        className="hero h-dvh"
+        style={{
+          backgroundImage: "url(./bg-home.jpg)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <div className="banner">
+              <div className="slider">
+                <div className="items"><img src="/1.avif" alt="" /> </div>
+              </div>
+            </div>
+            <div>
+              <Link
+                to={"/library"}
+                className="btn btn-nutral btn-outline rounded-3xl"
+              >
+                Go To Library
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
