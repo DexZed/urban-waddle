de4cab0bb55411c398b11d58c9097e93ea433793
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-30 15:12:35 +0600

    added custom buttons components

---
+++ assignment-4/src/components/Buttons.tsx
@@ -0,0 +1,12 @@
+type Props = {
+    name:string,
+    color:string,
+    disabled?:boolean,
+    action?: ()=>void
+};
+
+function ActionButtons({name,color,action,disabled=false}: Props) {
+  return <button disabled={disabled} onClick={action} className={"btn btn-outline  rounded-full "+`${color}`}>{name}</button>;
+}
+
+export default ActionButtons;
