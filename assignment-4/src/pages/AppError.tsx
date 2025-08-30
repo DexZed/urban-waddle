b19ce6a65cf59300158e072c1c12d5014228dcba
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-30 15:05:34 +0600

    `Rename Error component to AppError`

--- assignment-4/src/pages/AppError.tsx
+++ assignment-4/src/pages/AppError.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 
 
-export default function Error() {
+export default function AppError() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="bg-white shadow-lg rounded-xl p-6 max-w-md text-center border border-red-200">
---
+++ assignment-4/src/pages/Summary.tsx
@@ -0,0 +1,61 @@
+import { useGetBorrowSummaryQuery } from "../services/bookApiSlice";
+import AppError from "./AppError";
+
+function BorrowSummary() {
+  const {
+    data: summary,
+    error,
+    isLoading,
+  } = useGetBorrowSummaryQuery(undefined,
+    {
+      refetchOnMountOrArgChange: true,
+      refetchOnReconnect: true,
+      
+      
+    }
+  );
+
+  return (
+    <>
+      {error ? (
+        <>
+          <AppError></AppError>
+        </>
+      ) : isLoading ? (
+        <>
+          <div className="skeleton hero min-h-screen"></div>
+        </>
+      ) : (
+        <>
+            <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Title</th>
+                    <th>ISBN</th>
+                    <th>Total Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {summary?.data?.map((item, idx) => {
+                    return (
+                      <tr key={idx} className="hover:bg-base-300">
+                        <th>{idx}</th>
+                        <td>{item.book.title}</td>
+                        <td>{item.book.isbn}</td>
+                        <td>{item.totalQuantity}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </>
+      )}
+    </>
+  );
+}
+
+export default BorrowSummary;
