a7c6516c86c8dfdfeb739c89525981e68a782514
DexZed <144045080+DexZed@users.noreply.github.com>
2025-08-30 14:50:48 +0600

    `Added book form slice with update, prefill, and reset functionality`

---
+++ assignment-4/src/features/bookSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { Book, BookForm } from "../utils/Customtypes";
+
+interface BookFormState {
+  form: BookForm;
+}
+
+const initialState: BookFormState = {
+  form: {
+    title: "",
+    author: "",
+    genre: "FICTION",
+    isbn: "",
+    description: "",
+    copies: 0,
+    available: true,
+  },
+};
+
+export const bookFormSlice = createSlice({
+  name: "bookForm",
+  initialState,
+  reducers: {
+    // For updating individual fields
+    updateBookForm: <K extends keyof BookForm>(
+      state: BookFormState,
+      action: PayloadAction<{ field: K; value:BookForm[K] }>
+    ) => {
+      state.form[action.payload.field] = action.payload.value;
+    },
+    // Pre-filling form when editing a book
+    prefillBookForm: (state, action: PayloadAction<Book>) => {
+      const { updateAvailability, ...rest } = action.payload;
+      state.form = { ...rest };
+      
+    },
+     // For resetting form back to empty (useful after submission)
+     resetBookForm: (state) =>{
+      state.form = initialState.form;
+     }
+  },
+});
+
+export const { updateBookForm, prefillBookForm, resetBookForm } = bookFormSlice.actions;
+export default bookFormSlice.reducer;
